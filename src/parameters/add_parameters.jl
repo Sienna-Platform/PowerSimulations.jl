635c15854812f0563c56ab6f262a575d86c32056
Jose Daniel Lara <jdlara@berkeley.edu>
2026-04-15 15:21:07 -0600

    update handling of time series for branches
    Co-authored-by: jd-lara <16385323+jd-lara@users.noreply.github.com>

--- src/parameters/add_parameters.jl
+++ src/parameters/add_parameters.jl
@@ -256,7 +256,7 @@ function _add_time_series_parameters!(
             interval = ts_interval,
         )
     if isempty(device_name_axis)
-        @info "No devices with time series $ts_name found for $D devices. Skipping parameter addition."
+        @info " $ts_name found for $D devices. Skipping parameter addition."
         return
     end
     additional_axes = ()
