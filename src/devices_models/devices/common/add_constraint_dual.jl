88589d50ce2b6e1b75e38391170e040e3d7c7a9f
Youssef Miftah <miftahyo@gmail.com>
2026-09-05 09:39:51 +1000

    remove debug log

--- src/devices_models/devices/common/add_constraint_dual.jl
+++ src/devices_models/devices/common/add_constraint_dual.jl
@@ -185,7 +185,6 @@ function assign_dual_variable!(
 ) where {U <: PSY.System, V <: Union{AreaBalancePowerModel, AreaPTDFPowerModel}}
     time_steps = get_time_steps(container)
     existing = get_constraint(container, constraint_type(), PSY.Area)
-    @show existing
     area_names = axes(existing)[1]
     add_dual_container!(container, constraint_type, PSY.Area, area_names, time_steps)
     return
